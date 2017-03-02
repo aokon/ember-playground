a7d18dc50a8fbfbd45ae16c714777183a7589c9a
Adam Okoń <adam.okon@elegantcode.pl>
2017-03-02 11:10:29 +0100

    add animation between routes and after filtering the products

---
+++ super-rentals/app/transitions.js
@@ -0,0 +1,64 @@
+export default function() {
+  this.transition(
+    this.fromRoute('landing'),
+    this.toRoute('category'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('landing'),
+    this.toRoute('about'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('landing'),
+    this.toRoute('contact'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('category'),
+    this.toRoute('contact'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('category'),
+    this.toRoute('about'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('contact'),
+    this.toRoute('about'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('contact'),
+    this.toRoute('landing'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('about'),
+    this.toRoute('contact'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('about'),
+    this.toRoute('landing'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+}
