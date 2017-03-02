c76ab893521bc3e9428726b7daf37749362bf6c0
Adam Okoń <adam.okon@elegantcode.pl>
2017-03-02 15:00:52 +0100

    add customization for materialize css

--- super-rentals/tests/integration/components/product-filters-test.js
+++ super-rentals/tests/integration/components/product-filters-test.js
@@ -9,7 +9,6 @@ test('it invokes filter with valid rating value', function(assert) {
   const rating = 3;
 
   this.set('filter', (value) => {
-    console.log(value);
     assert.equal(value, rating, 'It sends proper value');
   });
 
